df795d1c04b45fc205453513261fb9a38bb3be5e
Utsho MH <mahadyhassanutsho@gmail.com>
2025-10-07 20:12:25 +0600

    added sort-functionalities in list

--- src/pages/book/Lists.jsx
+++ src/pages/book/Lists.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useLoaderData } from "react-router";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
@@ -8,8 +9,42 @@ export default function Lists() {
   const { readlist: readlistIds, wishlist: wishlistIds } = useLocalStorage();
   const books = useLoaderData();
 
-  const readlist = books.filter((book) => readlistIds.includes(book.id));
-  const wishlist = books.filter((book) => wishlistIds.includes(book.id));
+  const [readlist, setReadlist] = useState(
+    books.filter((book) => readlistIds.includes(book.id))
+  );
+  const [wishlist, setWishlist] = useState(
+    books.filter((book) => wishlistIds.includes(book.id))
+  );
+
+  const [sort, setSort] = useState();
+
+  const handleSort = (type) => {
+    setSort(type);
+  };
+
+  useEffect(() => {
+    switch (sort) {
+      case "Pages":
+        setReadlist((readlist) => [
+          ...readlist.sort((a, b) => a.totalPages - b.totalPages),
+        ]);
+        setWishlist((wishlist) => [
+          ...wishlist.sort((a, b) => a.totalPages - b.totalPages),
+        ]);
+        break;
+      case "Rating":
+        setReadlist((readlist) => [
+          ...readlist.sort((a, b) => a.rating - b.rating),
+        ]);
+        setWishlist((wishlist) => [
+          ...wishlist.sort((a, b) => a.rating - b.rating),
+        ]);
+        break;
+
+      default:
+        break;
+    }
+  }, [sort]);
 
   return (
     <Tabs>
@@ -21,6 +56,23 @@ export default function Lists() {
           Wishlist
         </Tab>
       </TabList>
+
+      <div className="dropdown dropdown-start mx-auto">
+        <div tabIndex={0} role="button" className="btn m-1">
+          Sort By: {sort === undefined ? "Default" : sort}
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
+        >
+          <li>
+            <button onClick={() => handleSort("Pages")}>Pages</button>
+          </li>
+          <li>
+            <button onClick={() => handleSort("Rating")}>Rating</button>
+          </li>
+        </ul>
+      </div>
 
       <TabPanel>
         <BookGrid books={readlist} />
